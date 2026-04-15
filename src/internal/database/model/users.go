2a0cdc4b01c98e18ef634d7c85d13201b8041c6a
rosfandy <bagusrosfandy@gmail.com>
2026-04-15 11:30:31 +0700

    feat: update pull commands model

--- src/internal/database/model/users.go
+++ src/internal/database/model/users.go
@@ -1,8 +1,9 @@
 package model
 
 type Users struct {
-	Id   int64   `gorm:"column:id;primaryKey" json:"id"`
-	Name *string `gorm:"column:name" json:"name"`
+	Id    int64   `gorm:"column:id;primaryKey" json:"id"`
+	Name  *string `gorm:"column:name" json:"name"`
+	Email *string `gorm:"column:email" json:"email"`
 }
 
 func (Users) TableName() string {
